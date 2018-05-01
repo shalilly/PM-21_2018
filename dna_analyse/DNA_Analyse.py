5f84a85ac5fef6b7debeb33783c342ff7452ac4c
shalilly <38620210+shalilly@users.noreply.github.com>
2018-05-01 19:38:50 +0200

    Update DNA_Analyse.py

--- dna_analyse/DNA_Analyse.py
+++ dna_analyse/DNA_Analyse.py
@@ -1,4 +1,10 @@
+
+
 gueltige_nukleotide = ['A', 'T', 'G', 'C']
+l_g = []
+l_a = []
+l_t = []
+l_c = []
 
 # Molekulargewichte in mg/mol
 # Nukleotidmonophosphate ohne 3'-OH-Gruppe
@@ -12,43 +18,81 @@ mw_oh = 17010
 
 def hole_dna_sequenz():
     """Liefere eine vom Nutzer erfragte und validierte DNA-Sequenz.
-
     Um gültig zu sein, darf die Sequenz nur aus den Buchstaben
     A, G, T, C, a, g, t, c bestehen.
     """
 
-    # Hinweis: Verwende eine while-Schleife, innerhalb derer Du eine
-    # Sequenz vom Nutzer erfragst und die Eingabe validierst.
-    # Nur wenn die Validierung klappt, brichst Du die Schleife ab
-    # und lieferst die Sequenz zurück.
+    # mithilfe von while-Schleife die Sequenz abfragen 
+    
+    x = 0
+    
     while True:
+        global sequence
         sequence = input('Gib eine DNA-Sequenz ein: ')
+       
+        # Überprüfen, ob alle eingegebenen Buchstaben Basen sind. 
+        # Wenn ja, müssen die Basen gezählt bzw. erst mal sortiert werden 
+        #  -> Listen 
+        # Wenn nicht: Fehlermeldung ausgeben und loop abbrechen 
         
-        # Hier fehlt natürlich noch die Validierung.
-        # Dafür brauchst Du noch eine Schleife, in der jeder Buchstabe
-        # der Eingabe auf Gültigkeit geprüft werden muss.
-        # Für einen Anfänger ist das schon eine ordentlich harte Nuss,
-        # die aber mit etwas Anstrengung zu knacken sein sollte.
+        if sequence[x] in gueltige_nukleotide: 
+            
+            while True: 
+            
+                if sequence[x] == gueltige_nukleotide[3]:
+                    l_g.append('G')
+                elif sequence[x] == gueltige_nukleotide[0]:
+                    l_a.append('A')
+                elif sequence[x] == gueltige_nukleotide[1]:
+                    l_t.append('T')
+                elif sequence[x] == gueltige_nukleotide[2]:
+                    l_c.append ('C')
+                else: 
+                    
+            # hier inneren loop abbrechen und äußeren wiederholen.
+            # aber nur bei dieser else Bedingung
+            # nicht, wenn der while-loop aus anderen Gründen abgebrochen wird
+            # nur wieeeeeeee ???????????????????????????? ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+            
+                x = x + 1 
+                if x == len(sequence):
+                    break
+                else: 
+                    continue
+                    
+                
+        else:            
+            print()
+            print('Bei der Eingabe handelt es sich' 
+                  'nicht um eine gültige DNA-Sequenz!')
+            print()
+            print()
+            continue
+       
+        return sequence
 
-        if erfolgreiche_validierung:
-            break
-        else:
-            # Dieser Teil wird ausgeführt wenn die Bedingung hinter if
-            # nicht erfüllt war.
-            print()
-            print(
-                'Bei der Eingabe handelt es sich nicht um eine gültige '
-                'DNA-Sequenz!'
-                )
-            print()
-            print()
-        
-    return sequence
-
+# Jetzt kommt die Ausführung der Definition 
 
 seq = hole_dna_sequenz()
-# Hier sollten jetzt die nötigen Berechnungen durchgeführt werden.
 
-# Und hier erfolgt dann die Ausgabe.
+# Dann werden zusätzliche Werte berechnet und/oder ausgegeben
+
 print()
-print('Eingelesene Sequenz:')
+print('Eingelesene Sequenz:') 
+print()
+print(sequence)
+print()
+print('Länge:' , len(sequence))
+print()
+print('Base    Häufigkeit') 
+print('G               ' , len(l_g))
+print('A               ' , len(l_a))
+print('T               ' , len(l_t))
+print('C               ' , len(l_c))
+print()
+print('% CG-Gehalt:' , 
+      len(l_g)+len(l_c)/(len(l_g)+len(l_c)+len(l_t)+len(l_a))*100)
+print()
+print('Molekulargewicht:' , 
+      (mw_g*len(l_g)+mw_a*len(l_a)+mw_t*len(l_t)+mw_c*len(l_c)+mw_oh)/1000 , 
+      'g/mol')
